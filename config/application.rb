5e22acc9bb73f7a3faabd06e475e77ba24147b81
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-03-24 07:54:26 +0900

    rspecのgenerator設定

--- config/application.rb
+++ config/application.rb
@@ -2,18 +2,19 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Projects
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+        # テストデータベースにレコードを作成するファイルの作成をスキップ
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
