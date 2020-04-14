84935aea966a0bf1ae4a1b0eb33ede893aa36ea8
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-15 00:25:21 +0900

    入れ子にしてファクトリの重複をなくす

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,20 @@ FactoryBot.define do
     description { "A test project." }
     due_on { 1.week.from_now }
     association :owner
+
+    # 昨日が締切のプロジェクト
+    factory :project_due_yesterday do
+      due_on { 1.day.ago }
+    end
+
+    # 今日が締切のプロジェクト
+    factory :project_due_today do
+      due_on { Date.current.in_time_zone }
+    end
+
+    # 明日が締切のプロジェクト
+    factory :project_due_tomorrow do
+      due_on { 1.day.from_now }
+    end
   end
 end
