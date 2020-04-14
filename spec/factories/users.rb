1f99ca2702e0edbd16db432a27f60306dad37a49
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-14 23:16:33 +0900

    ファクトリを使うように変更

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    first_name { "Aaron" }
+    last_name { "Sumner"}
+    email { "tester@examle.com" }
+    password { "dottle-nouveau-pavilion-tights-fuzre" }
+  end
+end
